43c043ba94f7ca0e5be5a751c48146a01edc4a2c
mikolaura <mikolaura58@gmail.com>
2023-12-06 20:00:38 +0200

    end

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import moment from "moment";
 import ErrorMessage from "./ErrorMessage";
 import { UserContext } from "../context/UserContext";
+import LeadModal from "./LeadModal";
 
 const Table = () => {
   const [token] = useContext(UserContext);
@@ -10,6 +11,25 @@ const Table = () => {
   const [loaded, setLoaded] = useState(false);
   const [activeModel, setActiveModel] = useState(false);
   const [id, setId] = useState(null);
+  const handleUpdate = async (id) => {
+    setId(id);
+    setActiveModel(true);
+  };
+  const handleDelete = async (id) => {
+    const requestOptions = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    };
+    const response = await fetch(`/api/leads/${id}`, requestOptions);
+    if (!response.ok) {
+      setErorMessage("Failed to delete lead");
+    }
+    getLeads();
+  };
+
   const getLeads = async () => {
     const requestOptions = {
       method: "GET",
@@ -30,38 +50,70 @@ const Table = () => {
   useEffect(() => {
     getLeads();
   }, []);
+  const handleModal = () => {
+    setActiveModel(!activeModel);
+    getLeads();
+    setId(null);
+  };
   return (
     <>
-      <button className="button is-fullwidth mb-5 is-primary ">
+      <LeadModal
+        active={activeModel}
+        handleModal={handleModal}
+        id={id}
+        token={token}
+        setErrorMessage={setErorMessage}
+      />
+      <button
+        className="button is-fullwidth mb-5 is-primary "
+        onClick={() => setActiveModel(true)}
+      >
         Create Lead
       </button>
       <ErrorMessage message={errorMessage} />
-      {loaded && leads ? <table className="table is-fullwidth">
-            <thead>
-                <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Company</th>
-                    <th>Email</th>
-                    <th>Note</th>
-                    <th>Last Updated</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                {leads.map(lead => (
-                    <tr key={lead.id}>
-                        <td>{lead.first_name}</td>
-                        <td>{lead.last_name}</td>
-                        <td>{lead.company}</td>
-                        <td>{lead.email}</td>
-                        <td>{lead.note}</td>
-                        <td>{lead.date_last_updated}</td>
-                        <td>{lead.note}</td>
-                    </tr>
-                ))}
-            </tbody>
-      </table> : <p>Loading</p>}
+      {loaded && leads ? (
+        <table className="table is-fullwidth">
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Company</th>
+              <th>Email</th>
+              <th>Note</th>
+              <th>Last Updated</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {leads.map((lead) => (
+              <tr key={lead.id}>
+                <td>{lead.first_name}</td>
+                <td>{lead.last_name}</td>
+                <td>{lead.company}</td>
+                <td>{lead.email}</td>
+                <td>{lead.note}</td>
+                <td>{moment(lead.date_last_updated).format("MMM Do YY")}</td>
+                <td>
+                  <button
+                    className="button mr-2 is-info is-light"
+                    onClick={() => handleUpdate(lead.id)}
+                  >
+                    Update
+                  </button>
+                  <button
+                    className="button mr-2 is-danger is-light"
+                    onClick={() => handleDelete(lead.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>Loading</p>
+      )}
     </>
   );
 };
